9f203a66983b76b9714a10193cf1ac29ab940757
PiotrD <piotr.draczkowski@scilifelab.se>
2025-12-15 15:34:32 +0100

    Fix echo statements for clarity in script

--- reconstruct_focused3Dclasses.sh
+++ reconstruct_focused3Dclasses.sh
@@ -4,15 +4,15 @@
 
 
 echo ' '
-echo 'This script will reconstruct full volumes of the imaged complex using the result of focused 3D classification.'
-echo 'IMPORTANT: This script should be executed from the Relion project directory (i.e. from which you execute relion command)'
+echo 'This script will reconstruct full volumes of the complex using the result of focused 3D classification.'
+echo 'IMPORTANT: This script should be executed from the Relion project directory (i.e. from which you execute the relion command)'
 
 # ask user for the folder with 3D classification results
 echo ' '
 echo 'specify the number of the 3D classification job you are aiming to reconstruct ./Class3D/job…? (example 084)' 
 read jobno
 
-#reads the most recent data.star in the specified Class3D folder
+#reads the most recent data. star in the specified Class3D folder
 datafile=$(ls -t Class3D/job$jobno/*data* | head -n1)
 
 #ask user which classes to reconstruct
@@ -51,7 +51,7 @@ then
 	echo "class $clA not specified"
 else
 echo ' '
-echo "NOTE:selects and reconstructs prtcls from class $clA"
+echo "NOTE: selects and reconstructs prtcls from class $clA"
 	
 	minvalA=$((clA - 1))'.9'
 	maxvalA=$clA'.1'
@@ -69,7 +69,7 @@ then
         echo "class $clB not specified"
 else
 echo ' '
-echo NOTE:selects and reconstructs prtcls from class $clB
+echo NOTE: selects and reconstructs prtcls from class $clB
 
         minvalB=$((clB - 1))'.9'
         maxvalB=$((clB))'.1'
@@ -87,7 +87,7 @@ then
         echo "class $clC not specified"
 else
 echo ' '
-echo NOTE:selects and reconstructs prtcls from class $clC
+echo NOTE: selects and reconstructs prtcls from class $clC
 
         minvalC=$((clC - 1))'.9'
         maxvalC=$clC'.1'
@@ -113,7 +113,7 @@ echo NOTE: selects and reconstructs prtcls from class $clD
         relion_star_handler --i $datafile --select rlnClassNumber --minval $minvalD --maxval $maxvalD --o class$clD'_job'$jobno.star
 
 	noprtclsD=$(grep '@' class$clD'_job'$jobno.star | wc -l)
-	echo NOTE:Number of particles in class $clD = $noprtclsD
+	echo NOTE: Number of particles in class $clD = $noprtclsD
 
         mpirun -n $mpi relion_reconstruct_mpi --i class$clD'_job'$jobno.star --ctf --maxres $maxres --angpix $angpix --sym $sym --o class$clD'_job'$jobno.mrc
 fi
@@ -123,7 +123,7 @@ then
         echo "class $clE not specified"
 else
 echo ' '
-echo NOTE:selects and reconstructs prtcls from class $clE
+echo NOTE: selects and reconstructs prtcls from class $clE
 
         minvalE=$((clE - 1))'.9'
         maxvalE=$clE'.1'
@@ -131,7 +131,7 @@ echo NOTE:selects and reconstructs prtcls from class $clE
         relion_star_handler --i $datafile --select rlnClassNumber --minval $minvalE --maxval $maxvalE --o class$clE'_job'$jobno.star
 
         noprtclsE=$(grep '@' class$clE'_job'$jobno.star | wc -l)
-        echo NOTE:Number of particles in class $clE = $noprtclsE
+        echo NOTE: Number of particles in class $clE = $noprtclsE
 
         mpirun -n $mpi relion_reconstruct_mpi --i class$clE'_job'$jobno.star --ctf --maxres $maxres --angpix $angpix --sym $sym --o class$clE'_job'$jobno.mrc
 fi 
@@ -141,7 +141,7 @@ then
         echo "class $clF not specified"
 else
 echo ' '
-echo NOTE:selects and reconstructs prtcls from class $clF
+echo NOTE: selects and reconstructs prtcls from class $clF
 
         minvalF=$((clF - 1))'.9'
         maxvalF=$clF'.1'
@@ -149,7 +149,7 @@ echo NOTE:selects and reconstructs prtcls from class $clF
         relion_star_handler --i $datafile --select rlnClassNumber --minval $minvalF --maxval $maxvalF --o class$clF'_job'$jobno.star
 
         noprtclsF=$(grep '@' class$clF'_job'$jobno.star | wc -l)
-        echo NOTE:Number of particles in class $clF = $noprtclsF
+        echo NOTE: Number of particles in class $clF = $noprtclsF
 
         mpirun -n $mpi relion_reconstruct_mpi --i class$clF'_job'$jobno.star --ctf --maxres $maxres --angpix $angpix --sym $sym --o class$clF'_job'$jobno.mrc
 fi
@@ -159,7 +159,7 @@ then
         echo "class $clG not specified"
 else
 echo ' '
-echo NOTE:selects and reconstructs prtcls from class $clG
+echo NOTE: selects and reconstructs prtcls from class $clG
 
         minvalG=$((clG - 1))'.9'
         maxvalG=$clG'.1'
@@ -167,7 +167,7 @@ echo NOTE:selects and reconstructs prtcls from class $clG
         relion_star_handler --i $datafile --select rlnClassNumber --minval $minvalG --maxval $maxvalG --o class$clG'_job'$jobno.star
 
         noprtclsG=$(grep '@' class$clG'_job'$jobno.star | wc -l)
-        echo NOTE:Number of particles in class $clG = $noprtclsG
+        echo NOTE: Number of particles in class $clG = $noprtclsG
 
         mpirun -n $mpi relion_reconstruct_mpi --i class$clG'_job'$jobno.star --ctf --maxres $maxres --angpix $angpix --sym $sym --o class$clG'_job'$jobno.mrc
 fi
@@ -177,7 +177,7 @@ then
         echo "class $clH not specified"
 else
 echo ' '
-echo NOTE:selects and reconstructs prtcls from class $clH
+echo NOTE: selects and reconstructs prtcls from class $clH
 
         minvalH=$((clH - 1))'.9'
         maxvalH=$clH'.1'
@@ -185,7 +185,7 @@ echo NOTE:selects and reconstructs prtcls from class $clH
         relion_star_handler --i $datafile --select rlnClassNumber --minval $minvalH --maxval $maxvalH --o class$clH'_job'$jobno.star
 
         noprtclsH=$(grep '@' class$clH'_job'$jobno.star | wc -l)
-        echo NOTE:Number of particles in class $clH = $noprtclsH
+        echo NOTE: Number of particles in class $clH = $noprtclsH
 
         mpirun -n $mpi relion_reconstruct_mpi --i class$clH'_job'$jobno.star --ctf --maxres $maxres --angpix $angpix --sym $sym --o class$clH'_job'$jobno.mrc
 fi
@@ -195,7 +195,7 @@ then
         echo "class $clI not specified"
 else
 echo ' '
-echo NOTE:selects and reconstructs prtcls from class $clI
+echo NOTE: selects and reconstructs prtcls from class $clI
 
         minvalI=$((clI - 1))'.9'
         maxvalI=$clH'.1'
@@ -203,7 +203,7 @@ echo NOTE:selects and reconstructs prtcls from class $clI
         relion_star_handler --i $datafile --select rlnClassNumber --minval $minvalI --maxval $maxvalI --o class$clI'_job'$jobno.star
 
         noprtclsI=$(grep '@' class$clI'_job'$jobno.star | wc -l)
-        echo NOTE:Number of particles in class $clI = $noprtclsI
+        echo NOTE: Number of particles in class $clI = $noprtclsI
 
         mpirun -n $mpi relion_reconstruct_mpi --i class$clI'_job'$jobno.star --ctf --maxres $maxres --angpix $angpix --sym $sym --o class$clI'_job'$jobno.mrc
 fi
@@ -213,7 +213,7 @@ then
         echo "class $clJ not specified"
 else
 echo ' '
-echo NOTE:selects and reconstructs prtcls from class $clJ
+echo NOTE: selects and reconstructs prtcls from class $clJ
 
         minvalJ=$((clH - 1))'.9'
         maxvalJ=$clJ'.1'
@@ -221,7 +221,7 @@ echo NOTE:selects and reconstructs prtcls from class $clJ
         relion_star_handler --i $datafile --select rlnClassNumber --minval $minvalJ --maxval $maxvalJ --o class$clJ'_job'$jobno.star
 
         noprtclsJ=$(grep '@' class$clJ'_job'$jobno.star | wc -l)
-        echo NOTE:Number of particles in class $clJ = $noprtclsJ
+        echo NOTE: Number of particles in class $clJ = $noprtclsJ
 
         mpirun -n $mpi relion_reconstruct_mpi --i class$clJ'_job'$jobno.star --ctf --maxres $maxres --angpix $angpix --sym $sym --o class$clJ'_job'$jobno.mrc
 fi
